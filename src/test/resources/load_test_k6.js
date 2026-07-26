6776956e6f9332773532b00aa4fd9a625351c656
Marcin Gliwinski <margliwinski@gmail.com>
2026-07-26 19:11:39 +0200

    fix: balanced test

--- src/test/resources/load_test_k6.js
+++ src/test/resources/load_test_k6.js
@@ -1,36 +1,43 @@
 import http from 'k6/http';
 import { check } from 'k6';
 
+// Konfiguracja obciążenia - wymuszamy 3000 zapytań na sekundę przez 30 sekund
 export const options = {
-  scenarios: {
-    constant_request_rate: {
-      executor: 'constant-arrival-rate',
-      rate: 3000,          
-      timeUnit: '1s',      
-      duration: '30s',     
-      preAllocatedVUs: 50, 
-      maxVUs: 500,         
+    scenarios: {
+        constant_request_rate: {
+            executor: 'constant-arrival-rate',
+            rate: 3000,
+            timeUnit: '1s',
+            duration: '30s',
+            preAllocatedVUs: 150,
+            maxVUs: 500,
+        },
     },
-  },
 };
 
 export default function () {
-  const payload = JSON.stringify({
-    userId: `user-${Math.floor(Math.random() * 1000000)}`
-  });
+    // Losujemy numer kuponu od 1 do 100
+    const randomCouponId = Math.floor(Math.random() * 100) + 1;
+    const couponCode = `TEST${randomCouponId}`;
+    
+    // Generujemy losowego użytkownika, żeby nie wpaść w limit 1 użycia na osobę
+    const randomUserId = `user-${Math.random().toString(36).substring(7)}`;
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Forwarded-For': '8.8.8.8', // Symulujemy poprawne IP dla kuponu z 'PL'
-    },
-  };
+    const url = `http://localhost:8080/api/v1/coupons/${couponCode}/redeem`;
+    const payload = JSON.stringify({
+        userId: randomUserId
+    });
 
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Forwarded-For': '8.8.8.8'
+        },
+    };
 
-  const res = http.post('http://localhost:8080/api/v1/coupons/SUMMER10/redeem', payload, params);
+    const res = http.post(url, payload, params);
 
-
-  check(res, {
-    'is status 200 or 409': (r) => r.status === 200 || r.status === 409,
-  });
+    check(res, {
+        'is status 200 or 409': (r) => r.status === 200 || r.status === 409,
+    });
 }
